6e7bf3f07c99add2a1596f07d66c048dd8b1f075
Stuart Solomon <sollys3@yahoo.com>
2024-12-09 15:30:43 -0500

    day 7 late

---
+++ day7/day7-part1.js
@@ -0,0 +1,36 @@
+import { readFileSync } from "fs";
+
+function changeSigns(map) {
+	return map.length === 0
+		? []
+		: map[0] === 0
+		? [1, ...map.slice(1)]
+		: [0, ...changeSigns(map.slice(1))];
+}
+
+const input = readFileSync("advent/2024/day7/day7-input.txt", {
+	encoding: "utf-8",
+})
+	.trim()
+	.split("\r\n")
+	.map(line => line.split(": "));
+
+let results = 0;
+
+for (let [targ, numline] of input) {
+	let nums = numline.split(" ").map(Number),
+		map = nums.slice(1).map(() => 0);
+	while (true) {
+		let proc = nums.reduce((pre, cur, ind) => {
+			return ind === 0 || map[ind - 1] === 0 ? cur + pre : cur * pre;
+		}, 0);
+		if (proc === parseInt(targ)) {
+			results += proc;
+			break;
+		}
+		if (!map.includes(0)) break;
+		map = changeSigns(map);
+	}
+}
+
+console.log(results);

fb83948e407c5c9d9f47de9fb7e2c08fe1c5f8c6
Stuart Solomon <sollys3@yahoo.com>
2024-12-24 11:05:52 -0500

    tried out dijkstra on day 16.1

--- day16/day16-part1.js
+++ day16/day16-part1.js
@@ -1,40 +1,50 @@
 import { readFileSync } from "fs";
 
-function broot(y, x, dir, record, score = 0, turns = 0, path = {}) {
-	if (score > 133584) return false;
-	if (record.get([y, x].toString()) <= score && turns === 0) return false;
-	let newpath = { ...path };
-	newpath[`${y},${x}`] = true;
-	if (input[y]?.[x] === "S" && dir === 3) return { score, path: newpath };
-	let [y1, x1] =
-		dir === 0
-			? [y - 1, x]
-			: dir === 1
-			? [y, x + 1]
-			: dir === 2
-			? [y + 1, x]
-			: [y, x - 1];
-	record.set([y, x].toString(), score);
-	let a =
-		input[y1]?.[x1] !== "#"
-			? broot(y1, x1, dir, record, score + 1, 0, newpath)
-			: false;
-	let [b, c] =
-		turns === 0
-			? [
-					broot(y, x, (dir + 1) % 4, record, score + 1000, 1, newpath),
-					broot(y, x, (dir + 3) % 4, record, score + 1000, 1, newpath),
-			  ]
-			: [false, false];
-	if (a || b || c) {
-		let p = Math.min(
-			a.score || Infinity,
-			b.score || Infinity,
-			c.score || Infinity
-		);
-		return [a, b, c].find(q => q.score === p);
+function direct(dir, y, x) {
+	return dir === 0
+		? [y - 1, x]
+		: dir === 1
+		? [y, x + 1]
+		: dir === 2
+		? [y + 1, x]
+		: [y, x - 1];
+}
+
+function dijkstra(start, goal) {
+	const visited = new Set(),
+		record = {},
+		queue = [{ coords: start, score: 0, dir: 1 }];
+
+	while (queue.length) {
+		queue.sort((a, b) => a.score - b.score);
+		let { coords, score, dir } = queue.shift();
+		let [y, x] = coords;
+
+		if (
+			score >= (record[`${y},${x},${dir}`] ?? Infinity) ||
+			visited.has(`${y},${x},${dir}`)
+		)
+			continue;
+
+		visited.add(`${y},${x},${dir}`);
+		if (y === goal[0] && x === goal[1]) return score;
+
+		record[`${y},${x},${dir}`] = score;
+
+		for (let d of [0, 1, 3]) {
+			let newDir = (dir + d) % 4,
+				newScore = record[`${y},${x},${dir}`] + (d === 0 ? 1 : 1001);
+			let [y1, x1] = direct(newDir, y, x);
+
+			if (!input[y1]?.[x1] || input[y1]?.[x1] !== "#") {
+				queue.push({
+					coords: [y1, x1],
+					score: newScore,
+					dir: newDir,
+				});
+			}
+		}
 	}
-	return false;
 }
 
 const input = readFileSync(import.meta.dirname + "/day16-input.txt", {
@@ -44,18 +54,14 @@ const input = readFileSync(import.meta.dirname + "/day16-input.txt", {
 	.split("\r\n")
 	.map(line => line.split(""));
 
-let goal,
-	record = new Map();
-endloop: for (let y = 0; y < input.length; y++) {
+let start, goal;
+for (let y = 0; y < input.length; y++) {
 	for (let x = 0; x < input[y].length; x++) {
-		if (input[y][x] === "E") {
-			goal = [y, x];
-			break endloop;
-		}
+		if (input[y][x] === "E") goal = [y, x];
+		else if (input[y][x] === "S") start = [y, x];
 	}
 }
-let [a, b] = [broot(...goal, 2, record), broot(...goal, 3, record)];
 
-let results = Math.min(a.score || Infinity, b.score || Infinity);
+let results = dijkstra(start, goal);
 
 console.log(results);

f159ff5619ba92115960505acd3b8cc31782657e
Stuart Solomon <sollys3@yahoo.com>
2025-01-02 04:35:18 -0500

    day 24 refine

--- day24/day24-part2.js
+++ day24/day24-part2.js
@@ -1,35 +1,28 @@
 import { readFileSync } from "fs";
 
 function adderFix(gates) {
-	let c = gates.find(g => {
-		g[0].includes("x00") && g[0].includes("y00") && g[0].includes("AND");
-	})[1];
 	const swtiched = new Set();
+	let c = gates.find(g => /(?=.*x00)(?=.*AND)(?=.*y00)/.test(g[0].join()))[1];
 
 	for (let i = 1; i < 45; i++) {
-		let string = i.toLocaleString("en-US", {
+		const string = i.toLocaleString("en-US", {
 			minimumIntegerDigits: 2,
 		});
-		let [x, y, z] = ["x" + string, "y" + string, "z" + string];
-		let hold1 = gates.find(
-			gate =>
-				gate[0].includes(x) &&
-				gate[0].includes(y) &&
-				gate[0].includes("AND")
+		const [x, y, z] = ["x" + string, "y" + string, "z" + string];
+
+		let hold1 = gates.find(gate =>
+			RegExp(`(?=.*${x})(?=.*AND)(?=.*${y})`).test(gate[0].join())
 		)[1];
 
-		let hold2 = gates.find(
-			gate =>
-				gate[0].includes(x) &&
-				gate[0].includes(y) &&
-				gate[0].includes("XOR")
+		let hold2 = gates.find(gate =>
+			RegExp(`(?=.*${x})(?=.*XOR)(?=.*${y})`).test(gate[0].join())
 		)[1];
 
 		let zgate = gates.find(gate => gate[1] === z && gate[0].includes("XOR"));
 
 		if (!zgate) {
 			let zgate1 = gates.find(
-				gate => gate[0].includes("XOR") && gate[0].includes(hold2)
+				gate => gate[0].includes(hold2) && gate[0].includes("XOR")
 			);
 			swtiched.add(z).add(zgate1[1]);
 			zgate = zgate1;
@@ -46,26 +39,19 @@ function adderFix(gates) {
 			c = temp;
 		}
 
-		let hold3 = gates.find(
-			gate =>
-				gate[0].includes(zgate[0][0]) &&
-				gate[0].includes(zgate[0][2]) &&
-				gate[0].includes("AND")
+		let hold3 = gates.find(gate =>
+			RegExp(`(?=.*${c})(?=.*AND)(?=.*${hold2})`).test(gate[0].join())
 		)?.[1];
-		c = gates.find(
-			gate =>
-				gate[0].includes(hold1) &&
-				gate[0].includes(hold3) &&
-				gate[0].includes("OR")
+
+		c = gates.find(gate =>
+			RegExp(`(?=.*${hold1})(?=.*OR)(?=.*${hold3})`).test(gate[0].join())
 		)?.[1];
 
 		if (!c) {
-			let h1try = gates.find(
-					gate => gate[0].includes(hold1) && gate[0].includes("OR")
-				),
-				h3try = gates.find(
-					gate => gate[0].includes(hold3) && gate[0].includes("OR")
-				);
+			const [h1try, h3try] = [hold1, hold3].map(hold =>
+				gates.find(gate => gate[0].includes(hold) && gate[0].includes("OR"))
+			);
+
 			if (!h1try) {
 				c = h3try[1];
 				swtiched
@@ -86,8 +72,8 @@ const gates = readFileSync(import.meta.dirname + "/day24-input.txt", {
 	encoding: "utf-8",
 })
 	.trim()
-	.split("\r\n\r\n")
-	.map(x => x.split("\r\n"))[1]
+	.split("\r\n\r\n")[1]
+	.split("\r\n")
 	.map(x => x.split(" -> ").map((x, i) => (i === 0 ? x.split(" ") : x)));
 
 const results = adderFix(gates);

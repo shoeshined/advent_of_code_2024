6e1b4e35ab4261fbbda6edf0c809681309d5b39d
Stuart Solomon <sollys3@yahoo.com>
2025-01-03 19:40:29 -0500

    day 22 tiny refine

--- day22/day22-part2.js
+++ day22/day22-part2.js
@@ -5,7 +5,7 @@ function prune(n) {
 }
 
 function hash(n, times, map) {
-	let list = [[n % 10, 0]],
+	let price = n % 10,
 		key = [],
 		reached = new Set();
 
@@ -13,17 +13,18 @@ function hash(n, times, map) {
 		n = prune((n * 64) ^ n);
 		n = prune(Math.floor(n / 32) ^ n);
 		n = prune((n * 2048) ^ n);
-		list.push([n % 10, (n % 10) - list.at(-1)[0]]);
-		key.push(list.at(-1)[1]);
+		key.push((n % 10) - price);
+		price = n % 10;
 
 		if (i > 2) {
 			let y = key.slice(-4).join("");
 			if (!reached.has(y)) {
 				reached.add(y);
-				map.set(y, map.get(y) + (n % 10) || n % 10);
+				map.set(y, price + (map.get(y) ?? 0));
 			}
 		}
 	}
+	return map;
 }
 
 const input = readFileSync(import.meta.dirname + "/day22-input.txt", {
@@ -33,8 +34,7 @@ const input = readFileSync(import.meta.dirname + "/day22-input.txt", {
 	.split("\r\n")
 	.map(x => parseInt(x));
 
-const map = new Map();
-input.forEach(line => hash(line, 2000, map));
+const map = input.reduce((prev, line) => hash(line, 2000, prev), new Map());
 
 const results = Math.max(...map.values());
 
